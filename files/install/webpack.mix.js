4c57d70831dc49484e160c3fe97215099fd289a5
Mirko Haaser <mirko@topred.media>
2022-05-27 11:27:40 +0200

    Fix path to glide and mobile navigationÄ

--- files/install/webpack.mix.js
+++ files/install/webpack.mix.js
@@ -3,7 +3,7 @@ const tailwindcss = require('tailwindcss');
 
 mix.js('resources/js/site.js', 'public/js')
     .vue()
-    .copy('node_moduldes/@glidejs/glide/dist', 'public/vendor/glidejs')
+    .copy('node_modules/@glidejs/glide/dist', 'public/vendor/glidejs')
     .sass('resources/css/site.scss', 'public/css')
     .options({
         processCssUrls: false,
